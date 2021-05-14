4f3bfb56bd1789965d259dfbaecdefabedd32079
“iamsims” <074bct541.simran@pcampus.edu.np>
2021-05-14 11:32:16 +0545

    Code structired and constraints tweaked for easier game

--- js/game.js
+++ js/game.js
@@ -1,25 +1,5 @@
-const cvs = document.getElementById("mycanvas");
-const ctx = cvs.getContext("2d");
-let frames =0;
 const DEGREE = Math.PI/180;
-
-const sprite = new Image();
-sprite.src = "img/sprite.png";
-
-const SCORE_S = new Audio();
-SCORE_S.src = "audio/sfx_point.wav"
-
-const FLAP = new Audio();
-FLAP.src = "audio/sfx_flap.wav"
-
-const HIT = new Audio();
-HIT.src = "audio/sfx_hit.wav"
-
-const SWOOSHING = new Audio();
-SWOOSHING.src = "audio/sfx_swooshing.wav"
-
-const DIE = new Audio();
-DIE.src = "audio/sfx_die.wav"
+let frames =0;
 
 const state ={
     current : 0,
@@ -60,127 +40,9 @@ cvs.addEventListener("click", function(evt){
                 state.current = state.getReady;
 
             }
-
     }
 });
 
-
-const bg= {
-    sX:0,
-    sY:0,
-    w:275,
-    h:226,
-    x:0,
-    y: cvs.height - 226,
-
-    draw: function(){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x+this.w, this.y, this.w, this.h);                  
-    }
-}
-
-
-const bird ={
-    animation:[
-        {sX:276, sY:112},
-        {sX:276, sY:139},
-        {sX:276, sY:164},
-        {sX:276, sY:139}
-    ],
-    x : 50,
-    y : 150,
-    w : 34,
-    h : 26,
-    radius : 12,
-
-    period: 10,
-
-    frame: 0,
-
-    speed: 0,
-    gravity: 0.25,
-    jump : 4.6,
-    rotation : 0,
-
-
-    draw : function(){
-        let bird = this.animation[this.frame];
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.rotation);
-        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h,- this.w/2,-this.h/2, this.w, this.h);
-        ctx.restore();
-    },
-
-
-    update : function(){
-        this.period = state.current == state.getReady? 10:5;
-        this.frame+= frames%this.period == 0 ? 1 : 0;
-        this.frame = this.frame%this.animation.length;
-
-        if(state.current == state.getReady){
-            this.y = 150;
-            this.rotation = 0* DEGREE;
-            this.speed=0;
-        }
-        else{
-            this.speed+= this.gravity;
-            this.y += this.speed;
-
-            if (this.y + this.h/2 >= cvs.height - fg.h){
-                this.y = cvs.height - fg.h - this.h/2;
-                if(state.current ==state.game){
-                    state.current = state.over; 
-                    DIE.play();
-                }
-            }
-
-            if (this.speed >= this.jump){
-                this.rotation = 25* DEGREE;
-                this.frame=1;
-            }
-
-            else { 
-                this.rotation = -25* DEGREE;
-            }
-
-        }
-
-    },
-
-    speedReset: function(){
-        this.speed=0;
-    },
-
-    flap: function(){
-        this.speed = -this.jump;
-    }
-
-    
-
-}
-
-const fg= {
-    sX : 276,
-    sY : 0,
-    w: 224,
-    h : 112,
-    x: 0,
-    y: cvs.height - 112,
-    dx: 2,
-
-    draw : function(){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x+this.w, this.y, this.w, this.h);
-    },
-
-    update : function(){
-        if(state.current ==state.game){
-            this.x = (this.x - this.dx)% (this.w/2);
-        }
-    }
-
-}
 
 const getReady ={
     sX : 0,
@@ -194,8 +56,7 @@ const getReady ={
 
         if(state.current == state.getReady){
             ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
-        }
-        
+        } 
     
     }
 }
@@ -233,94 +94,6 @@ const gameOver={
     }
 }
 
-const pipes ={
-    position:[],
-    top:{
-        sX: 553,
-        sY:0
-    },
-
-    bottom:{
-        sX: 502,
-        sY: 0,
-    },
-    w: 53,
-    h: 400,
-    gap: 85,
-    UPPERYLIMIT: -150,
-    LOWERYLIMIT: -370,
-    maxYPos: -150,
-    minYPos: -370,
-    lastY: 0,
-    dx: 2,
-    draw: function(){
-        for (let i =0; i< this.position.length;i++){
-            let p= this.position[i];
-            let topYPos = p.y;
-            let bottomYPos = p.y + this.h + this.gap;
-
-            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);
-            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);
-        }
-    },
-
-    update: function(){
-        if (state.current!== state.game) return;
-
-        if(frames%100==0){
-            this.position.push({
-                x: cvs.width,
-                y: Math.random() * (this.maxYPos- this.minYPos) + this.minYPos,
-            });
-            let lastY = this.position[this.position.length-1].y;
-            this.maxYPos= lastY + 80;
-            this.minYPos = lastY - 80;
-
-            if (this.maxYPos > this.UPPERYLIMIT){
-                this.maxYPos = this.UPPERYLIMIT;
-            }
-
-            if (this.minYPos < this.LOWERYLIMIT){
-                this.minYPos = this.LOWERYLIMIT;
-            }
-        }
-
-        for(let i =0; i<this.position.length; i++){
-            let p= this.position[i];
-            p.x -= this.dx;
-
-            let bottomPipeYPos = p.y + this.h + this.gap;
-
-            //COLLISION DETECTION
-            if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > p.y && bird.y - bird.radius < p.y + this.h ){
-                state.current = state.over;
-                HIT.play();
-            }
-
-            if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > bottomPipeYPos && bird.y - bird.radius < bottomPipeYPos + this.h ){
-                state.current = state.over;
-                HIT.play();
-            }
-
-            if (p.x+this.w <=0){
-                this.position.shift();
-                score.value++;
-                SCORE_S.play();
-
-                score.best = Math.max(score.value, score.best);
-                localStorage.setItem("best", score.best);
-            }
-
-        }
-
-
-    },
-
-    reset: function(){
-        this.position=[];
-    }
-}
-
 const score={
     best : parseInt(localStorage.getItem("best"))|| 0,
     value : 0,
@@ -352,7 +125,6 @@ const score={
     }
 
 } 
-
 
 function draw(){
     ctx.fillStyle= "#70c5ce";
